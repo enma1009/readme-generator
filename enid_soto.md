976960000ebc8b890d0101d62cd96794d6a4229d
enma1009 <enid.soto@gmail.com>
2020-03-03 16:59:52 -0500

    completed app functionality

---
+++ enid_soto.md
@@ -0,0 +1,47 @@
+# Readme gen
+
+<img src="https://avatars3.githubusercontent.com/u/29459503?v=4" alt="avatar" style="border-radius: 36px" width="60" />
+
+Author: Enid Soto
+
+## Description
+
+hsjkoopp
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+npm install
+
+## Usage
+
+aaaaaaaa
+
+## License
+
+This project is licensed under the MIT license.
+
+## Tests
+
+To run tests, run the following command:
+
+npm test
+
+## Questions
+
+[![GitHub license](https://img.shields.io/badge/GitHubUser-enma1009-orange)](https://api.github.com/users/enma1009)
+
+If you have any questions about the repo, open an issue or contact me directly [here](https://api.github.com/users/enma1009).
--- index.js
+++ index.js
@@ -1,40 +1,106 @@
 const inquirer = require("inquirer");
-//var fs = require('fs');
-//var GitHub = require('github-api');
+const fs = require('fs');
+const axios = require('axios');
 
 inquirer.prompt([
     {
         type: "input",
-        name: "username",
-        message: "What is your GitHub user name?"
-    },
-    {
-        type: "password",
-        name: "ghPassword",
-        message: "What is your GitHub password?"
+        name: "name",
+        message: "What is your name?"
     },
     {
         type: "input",
-        name: "projectTitle",
+        name: "gitname",
+        message: "What is your GitHub user name?"
+    },
+    {
+        type: "input",
+        name: "title",
         message: "What is the title of your project?"
     },
     {
         type: "input",
-        name: "projectDescription",
+        name: "description",
         message: "Please enter a brief description of the project:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please explain the usage for your project:"
     }
   ]).then(function(response) {
+    let {name,gitname,title,description,usage} = response;
+    let avatarUrl;
+    let email;
+    let queryUrl = `https://api.github.com/users/${gitname}`;
+    axios.get(queryUrl).then(
+        (response) => {
+            //console.log(response);
+            avatarUrl = response.data.avatar_url;
+            
+            if(email==null) {
+                email = ".";
+            } else {
+                email = ", or email me at " + response.data.email + ".";
+            };
 
-    console.log(JSON.stringify(response,null,2));
-    // console.log(response.username);
-    // console.log(response.ghPassword);
-    // console.log(response.projectTitle);
-    // console.log(response.projectDescription);
+            let filename = name.toLowerCase().split(' ').join('_') + ".md";
+            /*NOTE FOR DAVID OR CRISTIAN: I had to outdent the data because the readme was displaying wrong because of the tab spaces (this is my theory). I know the following code looks ugly, I AM SORRY GUYS*/
+let data = `# ${title}
 
-    // if (response.confirm === response.password) {
-    //   console.log("Success!");
-    // }
-    // else {
-    //   console.log("You forgot your password already?!");
-    // }
+<img src="${avatarUrl}" alt="avatar" style="border-radius: 36px" width="60" />
+
+Author: ${name}
+
+## Description
+
+${description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+npm install
+
+## Usage
+
+${usage}
+
+## License
+
+This project is licensed under the MIT license.
+
+## Tests
+
+To run tests, run the following command:
+
+npm test
+
+## Questions
+
+[![GitHub license](https://img.shields.io/badge/GitHubUser-${gitname}-orange)](${queryUrl})
+
+If you have any questions about the repo, open an issue or contact me directly [here](${queryUrl})${email}`;
+
+    fs.writeFile(filename, data, function(error) {
+        if(error) {
+            return console.log(error);
+        } 
+        console.log("Your README file has been generated.");
+    })
+        }
+    );
+    
   });
